f06f1431c89d086fade6395c7095840c1bd532ec
enbot <nimbotelho@gmail.com>
2020-04-30 20:15:34 -0300

    add status model/service

---
+++ src/models/status.model.ts
@@ -0,0 +1,11 @@
+export class Status {
+
+    readonly ok: number = 200;
+
+    readonly error: number = 500;
+
+    constructor() { }
+
+}
+
+export default Status;
---
+++ src/services/status.service.ts
@@ -0,0 +1,19 @@
+import Status from "../models/status.model";
+
+export class StatusService {
+
+    private _status = new Status();
+
+    constructor() { }
+
+    public get ok(): number {
+        return this._status.ok;
+    }
+
+    public get error(): number {
+        return this._status.error;
+    }
+
+}
+
+export default StatusService;
