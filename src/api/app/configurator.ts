93e603b63779967da30426e2bb36c6371ff53d94
enbot <nimbotelho@gmail.com>
2020-05-03 04:05:29 -0300

    updating configurator

--- src/api/app/configurator.ts
+++ src/api/app/configurator.ts
@@ -8,14 +8,48 @@ import errors from '../middlwares/errors';
 export namespace Configurator {
 
   export function run(app: Application, prefix: string): void {
+
     app.enable('trust proxy');
     app.use(cors({ allowedHeaders: '*' }));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    // app.use(express.static(path.join(__dirname, 'public')));
+
     app.use(prefix, Router.router());
+
     app.use(errors.notfound);
     app.use(errors.handler);
     app.use(errors.default);
+
+
+    // if(!isProduction) {  
+    //   app.use(errorHandler());
+    // }
+
+
+    // const jwt = require('jsonwebtoken');
+
+    // module.exports = (req, res, next) => {
+    //   try {
+    //     const token = req.headers.authorization.split(' ')[1];
+    //     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    //     const userId = decodedToken.userId;
+    //     if (req.body.userId && req.body.userId !== userId) {
+    //       throw 'Invalid user ID';
+    //     } else {
+    //       next();
+    //     }
+    //   } catch {
+    //     res.status(401).json({
+    //       error: new Error('Invalid request!')
+    //     });
+    //   }
+    // };
+
+    // https://stackoverflow.com/questions/34117754/middleware-that-run-before-any-routes
+    // https://expressjs.com/en/guide/using-middleware.html
+
   }
 
   export function listen(app: Application, port: number) {
--- src/api/app/environment.ts
+++ src/api/app/environment.ts
@@ -7,16 +7,23 @@ export namespace Environment {
   export const env = process.env.NODE_ENV || 'development';
 
   export const jwt = {
+
     secret: process.env.JWT_SECRET || 'supersecretapikey'
+
   };
 
   export const log = {
+
     level: process.env.LOG_LEVEL || 'silly'
+
   };
 
   export const api = {
+
     port: process.env.API_PORT ? parseInt(process.env.API_PORT) : 3000,
+
     prefix: process.env.API_PREFIX || '/api'
+
   };
 
 }
