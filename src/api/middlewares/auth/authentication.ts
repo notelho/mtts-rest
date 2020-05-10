4c0e8bc8cbd63e7a56a8ee9985d68d7a064be550
enbot <nimbotelho@gmail.com>
2020-05-09 22:38:52 -0300

    starting auth middleware

--- src/api/middlewares/auth/authentication.ts
+++ src/api/middlewares/auth/authentication.ts
@@ -1,18 +1,45 @@
-  // const jwt = require('jsonwebtoken');
+import { Request, Response, NextFunction } from 'express';
+import HttpErrorType from "../../../types/http-error.type";
+import jwt from 'jsonwebtoken';
 
-    // module.exports = (req, res, next) => {
-    //   try {
-    //     const token = req.headers.authorization.split(' ')[1];
-    //     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    //     const userId = decodedToken.userId;
-    //     if (req.body.userId && req.body.userId !== userId) {
-    //       throw 'Invalid user ID';
-    //     } else {
-    //       next();
-    //     }
-    //   } catch {
+export function authentication(req: Request, res: Response, next: NextFunction): void {
+
+  try {
+
+    const bearer = req.headers.authorization;
+
+    if (!bearer) {
+      throw new Error();
+    }
+
+    const token = bearer.split(' ')[1];
+
+    const isValid = (function (token: string): boolean {
+
+      //     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+      //     const userId = decodedToken.userId;
+
+      return true;
+
+    })(token);
+
+    if (!isValid) {
+
+      throw new Error('Invalid ')
+
+    }
+
+
+  } catch (error) {
+
     //     res.status(401).json({
     //       error: new Error('Invalid request!')
     //     });
-    //   }
-    // };
+
+  }
+
+  // next({ status: 404, name: 'Not Found', message: 'Not Found' } as HttpErrorType);
+
+}
+
+export default authentication;
