6e299c02f02fb05f2c0f39d34d249197169d19a6
enbot <nimbotelho@gmail.com>
2020-05-10 19:00:19 -0300

    feat: add commitlint, eslint, lint-staged and tslint

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  apps: [{
-    name: 'mtts-rest',
-    script: './dist/app.js',
-    // exec_mode: 'cluster',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
-  }]
+    apps: [{
+        name: 'mtts-rest',
+        script: './dist/app.js',
+        // exec_mode: 'cluster',
+        instances: 1,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '1G',
+    }]
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
-const uglifyes = require('uglify-es');
 const composer = require('gulp-uglify/composer');
+const uglifyes = require('uglify-es');
 const uglify = composer(uglifyes, console);
 
 gulp.task('minify', function () {
-  return gulp.src('dist/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('dist/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
-  return gulp.src('dist/', { read: false })
-    .pipe(clean());
+    return gulp.src('dist/', { read: false })
+        .pipe(clean());
 });
